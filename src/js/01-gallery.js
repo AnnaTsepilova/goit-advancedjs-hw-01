6fad3423700b18d71de9241cf1214fd0e26a6cb4
Anna Tsepilova <virgo.tsav@gmail.com>
2023-11-05 15:59:10 +0200

    add task-02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryRef = document.querySelector('.gallery');
+
+setGallery(galleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+});
+
+function createItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+ `;
+}
+
+function setGallery(galleryItems) {
+  galleryRef.innerHTML = galleryItems
+    .map(galleryItem => {
+      return createItem(galleryItem);
+    })
+    .join('');
+}
 // Change code below this line
 
 console.log(galleryItems);
