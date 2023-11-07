a9c8fc6426bf68c8558b52b86e35da0f7f1f8c7c
AnnaTsepilova <virgo.tsav@gmail.com>
2023-11-07 11:18:23 +0000

    Deploying to gh-pages from @ AnnaTsepilova/goit-advancedjs-hw-01@5549b9e24d0ba320d81fc144baa1d4398cd03a04 🚀

--- commonHelpers2.js
+++ commonHelpers2.js
@@ -1,2 +1,2 @@
-import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const n=document.querySelector("iframe"),e=new r(n),t="videoplayer-current-time",m=function({seconds:o}){localStorage.setItem(t,o)};e.on("timeupdate",a(m,1e3));const c=localStorage.getItem(t);e.setCurrentTime(c);
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as a}from"./assets/vendor-78be7656.js";const n=document.querySelector("iframe"),e=new r(n),t="videoplayer-current-time",m=function({seconds:o}){localStorage.setItem(t,o)};e.on("timeupdate",a(m,1e3));const c=localStorage.getItem(t)||0;e.setCurrentTime(c);
 //# sourceMappingURL=commonHelpers2.js.map
